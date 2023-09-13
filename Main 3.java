bfee8cf1cc734548ac1b522e737bc24f993da10e
Prabhnoorgithub <144378555+Prabhnoorgithub@users.noreply.github.com>
2023-09-13 13:59:37 -0400

    Add files via upload

---
+++ Main 3.java
@@ -0,0 +1,14 @@
+
+class Main {
+  public static void main(String[] args){ 
+  int x;//declartion 
+    x=4;// inittiization
+  int y;
+    y=7;
+  System.out.println(x + y);
+    System.out.println(x - y);
+    System.out.println(x / y);
+    System.out.println(x * y);
+
+  }
+    } 
