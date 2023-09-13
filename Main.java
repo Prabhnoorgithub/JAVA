a85107a38674b220f8eea0e4e44798e1a481b96f
Prabhnoorgithub <144378555+Prabhnoorgithub@users.noreply.github.com>
2023-09-13 13:03:02 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.print("T");
+    System.out.print("R");
+    System.out.print("I");
+    System.out.print("A");
+    System.out.print("N");
+    System.out.print("G");
+    System.out.print("L");
+    System.out.print("E");
+  }
+}
