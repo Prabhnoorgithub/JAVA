4581aa9f536713c2d4d81e1014f75706b4996033
Prabhnoorgithub <144378555+Prabhnoorgithub@users.noreply.github.com>
2023-09-25 13:50:16 -0400

    Create systemoperation.java

---
+++ systemoperation.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    int a=7;
+    System.out.println(" addition: "+(a+=1));
+     System.out.println(" subtraction: "+(a-=1));
+     System.out.println(" multiplication: "+(a*=2));
+     System.out.println(" division: "+(a/=2));
+  }
+}
