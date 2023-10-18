226064ffc91bf5291f97678b28eb71276c305408
Prabhnoorgithub <144378555+Prabhnoorgithub@users.noreply.github.com>
2023-10-18 13:43:35 -0400

    Create -loop.java

---
+++ -loop.java
@@ -0,0 +1,19 @@
+//looping statemnts
+class Main {
+  public static void main(String[] args) {
+
+
+    //while loop
+    int i = 15;
+    while (i > 9) {
+      System.out.println(i);
+      i--;//i=i+1 : i+=1 ; 1++
+    }
+
+    //for loop
+    /*for(int j=0 ;j<5 ; j++)
+    {
+      System.out.println(j);
+  }*/
+}
+}
